77f9d1de72e60e58ca102b720d77297138db1557
Khachatur <58373478+KhachaturB@users.noreply.github.com>
2020-03-21 14:39:57 +0300

    feature/gameworldservice
    подкорректировал файл Startup.cs

--- Services/GameWorldService/GameWorldService.Api/Startup.cs
+++ Services/GameWorldService/GameWorldService.Api/Startup.cs
@@ -1,17 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using GameWorldService.Domain.Logic;
-using Microsoft.OpenApi.Models;
+using GameWorldService.Logic;
 using GameWorldService.Logic.Clients;
 using GameWorldService.Logic.Services;
-using GameWorldService.Logic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace GameWorldService.Api
 {
@@ -20,7 +15,7 @@ namespace GameWorldService.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "GameWorld API", Version = "v0" });
@@ -47,12 +42,11 @@ namespace GameWorldService.Api
             });
 
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller}/{action}");
             });
         }
     }
